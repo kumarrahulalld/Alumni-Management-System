f6defddf94c04017f72a26c1a72ea6eee4f764e0
Adr <52234332+kumarrahulalld@users.noreply.github.com>
2020-03-29 06:02:07 +0530

    Update email.php

--- email.php
+++ email.php
@@ -11,12 +11,12 @@ $mail->Host = "smtp.gmail.com";
 //Set this to true if SMTP host requires authentication to send email
 $mail->SMTPAuth = true;                          
 //Provide username and password     
-$mail->Username = "kumarrahul.allduniv@gmail.com";                 
-$mail->Password = "Rm@0154772";                           
+$mail->Username = "email";                 
+$mail->Password = "password";                           
 //If SMTP requires TLS encryption then set it
 $mail->SMTPSecure = "tls";                           
 //Set TCP port to connect to 
 $mail->Port =587;                                   
-$mail->From = "kumarrahul.allduniv@gmail.com";
-$mail->FromName = "CCE | ALUMNI ";
+$mail->From = "email";
+$mail->FromName = "";
 ?>
